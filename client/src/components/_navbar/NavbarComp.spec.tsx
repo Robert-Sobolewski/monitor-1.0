7813177c31216e1e052bd354a20ed9f64e7d19ff
Robert-Sobolewski <84735293+Robert-Sobolewski@users.noreply.github.com>
2022-03-16 11:08:32 +0100

    added jest

---
+++ client/src/components/_navbar/NavbarComp.spec.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavbarComp from "./NavbarComp";
+import { render, screen } from "@testing-library/react";
+
+describe("test for navbar component", () => {
+  // render(<NavbarComp />);
+
+  test("if true is true", () => {
+    expect(true).toBe(true);
+  });
+});
